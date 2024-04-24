ba77725853e98f0fcbe4372f91663518f5d432bd
Jess <jessen2323@gmail.com>
2024-04-24 09:33:49 +0700

    Color from Hex String Function

---
+++ MappingJsonBundle/Model/ColorData.swift
@@ -0,0 +1,8 @@
+//
+//  ColorData.swift
+//  MappingJsonBundle
+//
+//  Created by User50 on 24/04/24.
+//
+
+import Foundation
---
+++ MappingJsonBundle/Model/ColorFromHex.swift
@@ -0,0 +1,8 @@
+//
+//  ColorFromHex.swift
+//  MappingJsonBundle
+//
+//  Created by User50 on 24/04/24.
+//
+
+import Foundation
