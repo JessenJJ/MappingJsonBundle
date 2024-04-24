c29ac1cc60ca90b50b290eef761d7d5de86cf1c7
Jess <jessen2323@gmail.com>
2024-04-24 08:52:03 +0700

    Initial Commit

---
+++ MappingJsonBundle/App/MappingJsonBundleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MappingJsonBundleApp.swift
+//  MappingJsonBundle
+//
+//  Created by User50 on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MappingJsonBundleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
